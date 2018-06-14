80d4b5ad12a5d8d0dc9ceba18341f15409797468
Peter Henry <peterhenry20@live.com>
2018-06-14 03:58:34 -0400

    Created Sample Project for Variable in Variable feature

---
+++ Samples/VariableInVariable/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using C9S.Extensions.Configuration;
+using Microsoft.Extensions.Configuration;
+
+namespace VariableInVariable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()    
+                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                .AddJsonFile($"appsettings.json", optional: true)
+                .AddEnvironmentVariables();
+
+            var configuration = builder.Build();
+
+            configuration.ResolveVariables();
+
+            Console.WriteLine(configuration.GetSection("Auth:ClientID"));
+        }
+    }
+}
