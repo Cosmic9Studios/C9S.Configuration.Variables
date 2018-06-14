53989b446f7e358a79c7cb9e8bbfc30df6d01189
Peter Henry <peterhenry20@live.com>
2018-06-14 01:23:38 -0400

    Added base files.

---
+++ C9S.Extensions.Configuration/ConfigurationRootExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace C9S.Extensions.Configuration
+{
+    public static class ConfigurationRootExtensions
+    {
+        public static void ResolveVariables(this IConfigurationRoot configuration)
+        {
+            foreach (var configSection in configuration.GetChildren())
+            {
+                foreach (Match match in variableRegex.Matches(configSection.Value ?? ""))
+                {
+                    var variable = match.Groups["var"].Value;
+                    var sections = new List<string>(variable.Split('.', System.StringSplitOptions.RemoveEmptyEntries));
+                    IConfigurationSection section = configSection;
+                    if (sections.Any())
+                    {
+                        section = Configuration.GetSection(sections[0]);
+                        var key = sections.Last();
+
+                        sections.RemoveAt(0);
+                        sections.RemoveAt(sections.Count - 1);
+                        foreach (var sec in sections)
+                        {
+                            section = section.GetSection(sec);
+                        }
+
+                        configSection.Value = configSection.Value.Replace($"{{{{{variable}}}}}", section[key]);
+                    }
+                }
+
+                ResolveVariables(configSection);
+            }
+        }
+    }
+}
